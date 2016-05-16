dda8a5540acb14d38ac40896407a9295c188f14b
Akos Kemives <akoskm@gmail.com>
2016-05-16 23:12:28 +0200

    restructured api

---
+++ src/api.js
@@ -0,0 +1,62 @@
+export default(app, logger) => {
+
+  let sendErr = function(res, msg) {
+    res.status(200).json({
+      success: false, message: msg
+    });
+  };
+
+  return {
+    write: function (req, res) {
+      let body = req.body;
+      let message;
+
+      if (body === null || typeof body !== 'object' || typeof body.log !== 'object') {
+        sendErr(res, 'request body is missing');
+      } else {
+        let log = body.log;
+        if (log.data === null || log.data === undefined) {
+          sendErr(res, 'message is missing from request body');
+        } else if (log.createdBy === null || log.createdBy === undefined) {
+          sendErr(res, 'createdBy is missing from request body');
+        } else {
+          message = {
+            data: log.data,
+            createdBy: log.createdBy,
+            level: log.level,
+            topic: log.topic
+          };
+
+          console.log(message);
+
+          app.db.models.Message.create(message, function(err, doc) {
+            if (err) {
+              logger.error(err);
+            }
+
+            logger.debug('message saved');
+
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json({
+              success: true, message: 'saved'
+            });
+          });
+        }
+      }
+    },
+    read: function (req, res) {
+      let skip = req.query.skip || 0;
+      let limit = req.query.limit || 100;
+
+      app.db.models.Message.find().skip(skip).limit(limit).sort('-createdBy').exec(function(err, messages) {
+        if (err) {
+          logger.error(err);
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(messages);
+      });
+    }
+  };
+}
+
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import http from 'http';
 import MessageSchema from './schema/Message';
 import log4js from 'log4js';
 import bodyParser from 'body-parser';
+import apiFactory from './api';
 
 // logger configuration
 log4js.configure('./config/log4js.json');
@@ -31,67 +32,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.db = mongoose.createConnection(config.mongodb.uri, mongoConfig);
 app.db.model('Message', MessageSchema(autoIndex));
 
-let sendErr = function(res, msg) {
-  res.status(200).json({
-    success: false, message: msg
-  });
-};
 
 app.get('/', function (req, res) {
   res.send('vrbose is running');
 });
 
-app.post('/write', function (req, res) {
-  let body = req.body;
-  let message;
+const api = apiFactory(app, logger);
 
-  if (body === null || typeof body !== 'object' || typeof body.log !== 'object') {
-    sendErr(res, 'request body is missing');
-  } else {
-    let log = body.log;
-    if (log.data === null || log.data === undefined) {
-      sendErr(res, 'message is missing from request body');
-    } else if (log.createdBy === null || log.createdBy === undefined) {
-      sendErr(res, 'createdBy is missing from request body');
-    } else {
-      message = {
-        data: log.data,
-        createdBy: log.createdBy,
-        level: log.level,
-        topic: log.topic
-      };
+app.post('/write', api.write);
 
-      console.log(message);
-
-      app.db.models.Message.create(message, function(err, doc) {
-        if (err) {
-          logger.error(err);
-        }
-
-        logger.debug('message saved');
-
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json({
-          success: true, message: 'saved'
-        });
-      });
-    }
-  }
-});
-
-app.get('/read', function (req, res) {
-  let skip = req.query.skip || 0;
-  let limit = req.query.limit || 100;
-
-  app.db.models.Message.find().skip(skip).limit(limit).sort('-createdBy').exec(function(err, messages) {
-    if (err) {
-      logger.error(err);
-    }
-
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(messages);
-  });
-});
+app.get('/read', api.read);
 
 app.server.listen(app.config.port, function(){
   logger.info('vrbose is running on port', app.config.port);
