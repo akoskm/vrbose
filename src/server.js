030788f65b76f7f5b0c041ffe5bae605a16c4de9
Akos Kemives <akoskm@gmail.com>
2016-05-25 21:28:03 +0200

    move the watcher test temporarily :construction:

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ import MessageSchema from './schema/Message';
 import WatcherSchema from './schema/Watcher';
 import log4js from 'log4js';
 import bodyParser from 'body-parser';
-import AppendWatcher from './appendwatcher';
 
 // logger configuration
 log4js.configure('./config/log4js.json');
@@ -40,74 +39,4 @@ app.get('/', function (req, res) {
 
 app.server.listen(app.config.port, function(){
   logger.info('vrbose is running on port', app.config.port);
-});
-
-let newWatcherCfg = {
-  id: 'INFO_AND_ERROR',
-  path: '.',
-  name: 'INFO and ERROR',
-  description: 'Watch for [INFO] and [ERROR] occurance in this file',
-  filename: 'myfile1.txt',
-  matchers: [{
-    name: 'INFO',
-    regex: /\[INFO\]/
-  }, {
-    name: 'ERROR',
-    regex: /\[ERROR\]/
-  }]
-};
-
-mongoose.model('Watcher').find(function (err, docs) {
-  if (err) {
-    throw err;
-  }
-  if (!docs || docs.length < 1) {
-    mongoose.model('Watcher').create(newWatcherCfg, function (err, doc) {
-      if (err) throw err
-      console.log(doc);
-    });
-  }
-  let watcher = docs[0];
-
-  const appendWatcher = AppendWatcher.watch(watcher.filename, watcher.endPos);
-  let counter = 0;
-  appendWatcher
-    .on('append', function (message, endPos) {
-      if (message) {
-        var matches = newWatcherCfg.matchers.map(function (m) {
-          var result = message.match(m.regex);
-          return {
-            name: m.name,
-            regex: m.regex,
-            count: result ? result.length : 0
-          }
-        });
-        var updateMatchers = matches.map(function (mr) {
-          return {
-            query: {
-              'id': newWatcherCfg.id,
-              'matchers.name': mr.name
-            },
-            update: {
-              $inc: {
-                'total': mr.count,
-                'matchers.$.count': mr.count
-              },
-              $set: {
-                endPos
-              }
-            }
-          };
-        });
-        updateMatchers.forEach(function (um) {
-          mongoose.model('Watcher').update(um.query, um.update, function (err, doc) {
-            if (err) throw err;
-            console.log(doc);
-          });
-        });
-      }
-    })
-    .on('error', function (err) {
-      throw err;
-    });
 });
---
+++ src/watchertest.js
@@ -0,0 +1,74 @@
+import mongoose from 'mongoose';
+import AppendWatcher from './appendwatcher';
+
+export default() => {
+  let newWatcherCfg = {
+    id: 'INFO_AND_ERROR',
+    path: '.',
+    name: 'INFO and ERROR',
+    description: 'Watch for [INFO] and [ERROR] occurance in this file',
+    filename: 'myfile1.txt',
+    matchers: [{
+      name: 'INFO',
+      regex: /\[INFO\]/
+    }, {
+      name: 'ERROR',
+      regex: /\[ERROR\]/
+    }]
+  };
+
+  mongoose.model('Watcher').find(function (err, docs) {
+    if (err) {
+      throw err;
+    }
+    if (!docs || docs.length < 1) {
+      mongoose.model('Watcher').create(newWatcherCfg, function (err, doc) {
+        if (err) throw err
+        console.log(doc);
+      });
+    }
+    let watcher = docs[0];
+
+    const appendWatcher = AppendWatcher.watch(watcher.filename, watcher.endPos);
+    let counter = 0;
+    appendWatcher
+      .on('append', function (message, endPos) {
+        if (message) {
+          var matches = newWatcherCfg.matchers.map(function (m) {
+            var result = message.match(m.regex);
+            return {
+              name: m.name,
+              regex: m.regex,
+              count: result ? result.length : 0
+            }
+          });
+          var updateMatchers = matches.map(function (mr) {
+            return {
+              query: {
+                'id': newWatcherCfg.id,
+                'matchers.name': mr.name
+              },
+              update: {
+                $inc: {
+                  'total': mr.count,
+                  'matchers.$.count': mr.count
+                },
+                $set: {
+                  endPos
+                }
+              }
+            };
+          });
+          updateMatchers.forEach(function (um) {
+            mongoose.model('Watcher').update(um.query, um.update, function (err, doc) {
+              if (err) throw err;
+              console.log(doc);
+            });
+          });
+        }
+      })
+      .on('error', function (err) {
+        throw err;
+      });
+  });
+};
