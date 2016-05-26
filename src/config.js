6e910d5308b88f80372a750abafca126194c2cb2
Akos Kemives <akoskm@gmail.com>
2016-05-26 19:38:56 +0200

    crypto config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
+import path from 'path';
+
 let config = {};
 
+config.workingDir = path.join(__dirname, '..');
 config.port = process.env.PORT || 3006;
 config.mongodb = {
   // vrbose db, user and a messages collection has to be created
@@ -7,6 +10,12 @@ config.mongodb = {
   // db.createUser({"user": "vrbose", "pwd": "vrbose", "roles": [{role: "readWrite", db: "vrbose"}]});
   // db.createCollection("messages");
   uri: process.env.MONGO_URI || 'mongodb://vrbose:vrbose@localhost/vrbose'
+};
+config.cryptoKey = 'milutth3c4t';
+config.loginAttempts = {
+  forIp: 50,
+  forIpAndUser: 7,
+  logExpiration: '20m'
 };
 
 export default config;
