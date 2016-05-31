9c2fdbddb713534d38a7ceaf9afbf873ec7f1a88
Akos Kemives <akoskm@gmail.com>
2016-05-31 22:00:37 +0200

    inline edit

--- src/components/watchers/Summary.js
+++ src/components/watchers/Summary.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
+import classNames from 'classnames';
+
 import Col from 'react-bootstrap/lib/Col';
 import Row from 'react-bootstrap/lib/Row';
+import Form from 'react-bootstrap/lib/Form';
 import Table from 'react-bootstrap/lib/Table';
 import Button from 'react-bootstrap/lib/Button';
 import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import FormControl from 'react-bootstrap/lib/FormControl';
+
+import MatcherRow from './MatcherRow';
 
 class Summary extends React.Component {
+
   constructor(props) {
     super(props);
+
+    this.state = {
+      editing: false
+    };
 
     this.onNew = this.onNew.bind(this);
     this.onEdit = this.onEdit.bind(this);
@@ -20,7 +32,9 @@ class Summary extends React.Component {
   }
 
   onEdit() {
-
+    this.setState({
+      editing: true
+    });
   }
 
   onDelete() {
@@ -28,72 +42,48 @@ class Summary extends React.Component {
   }
 
   render() {
+    let matcherRows = <tr><td></td></tr>;
     let matchersTable = <Col xs={12} md={12} lg={12}>No entries found</Col>;
     if (this.props.matchers && this.props.matchers.length > 0) {
+      matcherRows = this.props.matchers.map((m) => {
+        return <MatcherRow matcher={m} watcherId={this.props.watcherId}/>;
+      });
       matchersTable = (
-      <Col xs={12} md={12} lg={12}>
-        <h4>Matcher
-          <Button
-            type='button'
-            bsStyle='primary'
-            className='pull-right btn-sm'
-            onClick={this.onNew}
-          >
-            New
-          </Button>
-        </h4>
-        <Table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Regex</th>
-              <th>Count</th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-          {this.props.matchers.map((m) => {
-            return (
+        <Col xs={12} md={12} lg={12}>
+          <h4>Matcher
+            <Button
+              type='button'
+              bsStyle='primary'
+              className='pull-right btn-sm'
+              onClick={this.onNew}
+            >
+              New
+            </Button>
+          </h4>
+          <Table>
+            <thead>
               <tr>
-                <td>{m.name}</td>
-                <td>{m.regex}</td>
-                <td>{m.count}</td>
-                <td>
-                  <Button
-                    type='button'
-                    bsStyle='primary'
-                    className='btn-xs'
-                    onClick={this.onEdit}
-                    title='Edit'
-                  >
-                    <Glyphicon glyph='pencil'/>
-                  </Button>
-                </td>
-                <td>
-                  <Button
-                    type='button'
-                    bsStyle='danger'
-                    className='btn-xs'
-                    onClick={this.onEdit}
-                    title='Edit'
-                  >
-                    <Glyphicon glyph='remove'/>
-                  </Button>
-                </td>
+                <th>Name</th>
+                <th>Regex</th>
+                <th>Count</th>
+                <th></th>
+                <th></th>
               </tr>
-            );
-          })}
-          </tbody>
-        </Table>
-      </Col>);
+            </thead>
+            <tbody>
+            {matcherRows}
+            </tbody>
+          </Table>
+        </Col>
+      );
     }
     return matchersTable;
   }
 }
 
 Summary.propTypes = {
-  matchers: React.PropTypes.object.isRequired
+  matchers: React.PropTypes.object.isRequired,
+  watcherId: React.PropTypes.object.isRequired
 };
 
 export default Summary;
